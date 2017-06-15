c5f8357311dd9ebcfce44ef7bf378ad326d4a28f
Alexandre Fayolle <alexandre.fayolle@camptocamp.com>
2017-06-15 17:12:04 +0200

    fix modules and migration script

--- odoo/songs/upgrade/10_0_17/account_analytic_dimension.py
+++ odoo/songs/upgrade/10_0_17/account_analytic_dimension.py
@@ -18,14 +18,20 @@ def create_dimensions(ctx):
     dim_xmlid = '__setup__.dimension_business_unit'
     create_or_update(ctx, 'account.analytic.dimension', dim_xmlid, dim_vals)
 
-    tag_names = ('Admin', 'R&D', 'Sales & mkg', 'Sales', 'Composite',
-                 'Engineering', 'Process')
-    for tag in tag_names:
+    tag_names = [('Admin', 'analytic_tag_admin'),
+                 ('R&D', 'analytic_tag_rd'),
+                 ('Sales & mkg', 'analytic_tag_sales_marketing'),
+                 ('Sales', 'analytic_tag_sales'),
+                 ('Composite', 'analytic_tag_composite'),
+                 ('Engineering', 'analytic_tag_engineering'),
+                 ('Process', 'analytic_tag_process'),
+                 ]
+    for tag, tag_xmlid in tag_names:
         tags_vals = {
             'name': tag,
             'analytic_dimension_id': ctx.env.ref(dim_xmlid).id,
         }
-        tag_xmlid = '__setup__%s' % tag.lower()
+        tag_xmlid = '__setup__.%s' % tag_xmlid
         create_or_update(ctx, 'account.analytic.tag', tag_xmlid, tags_vals)
 
 
