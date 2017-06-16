3db009db8b88791344f608b3e0fbb5255f9eb16a
Alexandre Fayolle <alexandre.fayolle@camptocamp.com>
2017-06-16 11:51:29 +0200

    fix module name and migration script

--- odoo/local-src/specific_analytic_tag_dimension/__manifest__.py
+++ odoo/local-src/specific_analytic_tag_dimension/__manifest__.py
@@ -2,7 +2,7 @@
 # Copyright 2017 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
-    "name": "RocTool specific sale module",
+    "name": "RocTool specific analytic module",
     "version": "10.0.1.0.0",
     "depends": [
         'sale',
