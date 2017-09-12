45eb8f9d69260340f03e965e80dacaaf77f6f170
Alexandre Fayolle <alexandre.fayolle@camptocamp.com>
2017-09-12 17:37:20 +0200

    tool to help import SNs with partner
    when creating a prodlot with a partner, create a move from Inventory Loss
    to Customers forcing the quant to place it on the correct location

--- odoo/local-src/specific_purchase/models/stock_production_lot.py
+++ odoo/local-src/specific_purchase/models/stock_production_lot.py
@@ -3,7 +3,7 @@
 # Copyright 2017 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo import models, api, fields
+from odoo import models, api, fields, _
 from dateutil.relativedelta import relativedelta
 
 
@@ -30,14 +30,19 @@ class StockProductionLot(models.Model):
         compute='_get_customer_warranty',
         store=True
     )
+    current_partner_id = fields.Many2one(
+        'stock.location',
+        compute='_get_current_partner',
+        inverse='_set_current_partner',
+    )
     _sql_constraints = [
         ('positive_supplier_warranty',
          'CHECK (supplier_warranty_duration >= 0)',
-         'The supplier warranty duration must be positive',
+         _('The supplier warranty duration must be positive'),
          ),
         ('positive_customer_warranty',
          'CHECK (customer_warranty_duration >= 0)',
-         'The customer warranty duration must be positive',
+         _('The customer warranty duration must be positive'),
          )
         ]
 
@@ -85,3 +90,39 @@ class StockProductionLot(models.Model):
                 )
                 end = start + delta
                 rec.date_customer_warranty_end = fields.Date.to_string(end)
+
+    @api.depends('quant_ids.history_ids.partner_id')
+    def _get_current_partner(self):
+        for rec in self:
+            if rec.quant_ids:
+                moves = rec.quant_ids[-1].history_ids
+                rec.current_partner_id = moves[-1].partner_id
+            else:
+                rec.current_partner_id = False
+
+    def _set_current_partner(self):
+        loc_inv_loss = self.env.ref('stock.location_inventory')
+        loc_customers = self.env.ref('stock.stock_location_customers')
+        for rec in self:
+            partner = rec.current_partner_id
+            if rec.quant_ids:
+                moves = rec.quant_ids[-1].history_ids
+                if moves[-1].partner_id == partner:
+                    continue
+                else:
+                    raise ValueError(
+                        _('Cannot change the partner the S/N')
+                    )
+            else:
+                move = self.env['stock.move'].create(
+                    {'name': 'set a S/N on a customer',
+                     'product_id': rec.product_id.id,
+                     'product_uom': rec.product_id.uom_id.id,
+                     'product_uom_qty': 1,
+                     'restrict_lot_id': rec.id,
+                     'location_id': loc_inv_loss.id,
+                     'location_dest_id': loc_customers.id,
+                     'partner_id': partner.id,
+                     }
+                )
+                move.action_done()
