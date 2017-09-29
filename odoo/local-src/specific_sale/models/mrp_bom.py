be3d73a92e529147704a15a9aed5886a3d1dae31
mpanarin <m.panarin@mobilunity.com>
2017-09-29 11:28:58 +0300

    [add] feature to create MO from BOM via button BSRTL-242

--- odoo/local-src/specific_sale/models/mrp_bom.py
+++ odoo/local-src/specific_sale/models/mrp_bom.py
@@ -4,6 +4,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from odoo import models, fields
+from odoo.exceptions import ValidationError
 
 
 class MrpBom(models.Model):
@@ -19,3 +20,24 @@ class MrpBom(models.Model):
         related='project_task_id.project_id',
         readonly=True,
     )
+
+    def create_manufacture_order(self):
+        self.ensure_one()
+        if not all((self.picking_type_id, self.product_id)):
+            raise ValidationError(
+                "Please specify Product variant and Operation"
+            )
+        manufacture_order = self.env['mrp.production'].create({
+            'bom_id': self.id,
+            'product_id': self.product_id.id,
+            'product_uom_id': self.product_uom_id.id,
+            'picking_type_id': self.picking_type_id.id,
+        })
+        return {
+            'view_type': 'form',
+            'view_mode': 'form',
+            'res_model': 'mrp.production',
+            'target': 'current',
+            'res_id': manufacture_order.id,
+            'type': 'ir.actions.act_window'
+        }
