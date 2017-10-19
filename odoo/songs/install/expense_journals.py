e4a78f7f7bce3f56387ac5a284891a26a30772ca
mpanarin <m.panarin@mobilunity.com>
2017-10-19 17:58:49 +0300

    [FIX] for the song from pr 116. was crashing if migration rerun

--- odoo/songs/install/expense_journals.py
+++ odoo/songs/install/expense_journals.py
@@ -3,11 +3,11 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
 
 import anthem
+from anthem.lyrics.records import create_or_update
 
 
 @anthem.log
 def create_journals(ctx):
-    journal = ctx.env["account.journal"]
     companies = ctx.env["res.company"].search([])
     for company in companies:
         values = {
@@ -16,7 +16,13 @@ def create_journals(ctx):
             "company_id": company.id,
             "code": "EXP",
         }
-        company.expense_journal_id = journal.create(values)
+        company.expense_journal_id = create_or_update(
+            ctx, "account.journal",
+            "account.expense_journal_{}".format(
+                company.name.lower().replace(" ", "_").replace(".", "")
+            ),
+            values
+        )
 
 
 @anthem.log
